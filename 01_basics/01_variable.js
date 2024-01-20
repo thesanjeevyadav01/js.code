860f1cc357ae490477a0180d21bc7d747fa19eee
Sanjeevkumar Yadav <123267070+thesanjeevyadav01@users.noreply.github.com>
2024-01-20 20:06:29 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 12234
+var accountName = "Sanjeev"
+let accountEmail = "sanjeevyadav47712@gmail.com"
+
+accountCity = "Mumbai"
+
+accountCity = "Benguluru"
+
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountName,accountCity])
+
+/*
+Prefer not to use var--- bcaz of issue in block scope and functional scope
+
+*/
