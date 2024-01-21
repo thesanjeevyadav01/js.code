19664d7dff8d365bab574f0b93ffdce14b4cb4c0
Sanjeevkumar Yadav <123267070+thesanjeevyadav01@users.noreply.github.com>
2024-01-21 19:59:27 +0000

    DataType

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer javascript version 
+
+//  alert(3+3)  --->> As we are using node not browser
+
+console.log("Sanjeev");  console.log("Yadav")   // This is not an appropriate manner to write code // USe new line for every new sentence.
+//console.log("Sanjeev") console.log("Yadav")   // This will show error as semi colon is not added after first sentence completion  
+// Code in Javascript should be in reability 
+
+
+
+
+
+// number =>>> 2 to power 53
+// bigInt =>> for very big numbers , i.e use din stocks and big tech companies
+// string =>>> ""
+// boolean =>> true/false
+
+// null =>> standalone value 
+// undefined =>> 
+//symbol =>> for uniqueness we use symbol 
+
+
+
+//object =>
+console.log(typeof undefined) // >>>  undefined 
+console.log(typeof null) // >>>  object 
+
